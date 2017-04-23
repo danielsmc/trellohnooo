7bf5295339e35213f9f3e71e090729f7d6103ba5
Eric Bailey <eric.w.bailey@gmail.com>
2017-04-23 13:58:23 -0400

    Defucked up dog alert

--- src/script/components/Alert.js
+++ src/script/components/Alert.js
@@ -9,8 +9,8 @@ const Alert = ({text,headline,special}) => {
     <button className="alert-close" onClick={clearAlert}>
       <svg class="alert-close-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21"><path fill="#ffffff" fill-rule="evenodd" d="M1.53 9.01L6 11.42l-2.41 4.47c-.74 1.487-.164 3.292 1.298 4.08 1.462.788 3.285.274 4.12-1.16l2.41-4.47 4.47 2.41c1.498.805 3.364.245 4.17-1.25.807-1.497.247-3.364-1.25-4.17l-4.47-2.41 2.41-4.47c.74-1.488.165-3.293-1.297-4.08-1.46-.79-3.284-.275-4.12 1.16L8.92 6 4.45 3.59C2.96 2.85 1.156 3.425.37 4.887-.42 6.35.094 8.173 1.53 9.008z"/></svg>
     </button>
+    <p>{headline}</p>
     <p className="alert-text">{text}</p>
+    {dog}
     </div>;
 }
-
-export default Alert;
