18a15a4d3debfe6547f84b3db909c46c7a9227cd
Daniel McLaughlin <danielsmc@gmail.com>
2017-04-23 14:37:03 -0400

    card-beingdragged

--- src/script/components/Card.js
+++ src/script/components/Card.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { DragSource } from 'react-dnd';
+import classNames from 'classnames';
 import Meter from './Meter';
 
 const cardSource = {
@@ -18,8 +19,9 @@ function collect(connect, monitor) {
 class Card extends Component {
   render() {
     const { connectDragSource, isDragging, id, name, effort, value, age, work_done } = this.props;
+    const classes = classNames({card: true, 'card-beingdragged':isDragging});
     return connectDragSource(
-		<div className="card">
+		<div className={classes}>
       <div className="card-title">Card {id} <span className="card-age">{Math.floor(age)} days old</span></div>
 			<div className="card-effort"><strong>{effort}</strong> effort</div>
       <div className="card-value"><strong>{value}</strong> pts</div>
